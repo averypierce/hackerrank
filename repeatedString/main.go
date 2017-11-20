11bfe39cff413100ed915442befb53ace948f651
Avery Pierce <fredknex@gmail.com>
2017-11-19 16:59:32 -0800

    hackerrank implementation/repeatedString

---
+++ repeatedString/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	s := input.Text()
+	input.Scan()
+	n, _ := strconv.Atoi(input.Text())
+
+	//first, count all the a's in s
+	count := strings.Count(s, "a")
+	//Then multiply for full repetitions of string
+	total := count * int(math.Floor(float64(n/len(s))))
+	//Add remainder
+	total += strings.Count(s[:n%len(s)], "a")
+
+	fmt.Println(total)
+}
